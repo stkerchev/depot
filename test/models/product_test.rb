719dc2c5494bc30dc589c2010fcfdbcc6e7d0f09
stkerchev <stoyan.kerchev@gmail.com>
2023-06-17 22:35:17 +0300

    update Product test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,9 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+  end
 end

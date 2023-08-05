d5d61fe6904ea54617a853f6091916f5868cf3d0
stkerchev <stoyan.kerchev@gmail.com>
2023-08-05 23:06:08 +0300

    D1: Finding a Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_201957) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_05_195814) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"

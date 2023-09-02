133ef4b19b7b1790c3a6be42ef0faaf62eac93a7
stkerchev <stoyan.kerchev@gmail.com>
2023-09-02 23:55:49 +0300

    Revert "F4: Broadcasting Updates with Action Cable"
    This reverts commit eb144e13e35404bbf48f9bc8557eb487ff1396d2.

--- app/channels/product_channel.rb
+++
@@ -1,9 +0,0 @@
-class ProductChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "products"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end

14b74965aba24618bba7e7cb61b8ab9d2a78b18d
stkerchev <stoyan.kerchev@gmail.com>
2023-09-16 23:56:06 +0300

    G2: Adding Fields Dynamically to a Form - fixes

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
     showAdditionalFields() {
         let selection = this.selectionTarget.value
         for (let fields of this.additionalFieldsTargets) {
-            fields.disabled = fields.hidden = (fields.dataset.type != selection)
+            fields.disabled = fields.hidden = (fields.dataset.type !== selection)
         }
     }
 }

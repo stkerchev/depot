e865406c676a41db2dbd3e77a5d0e771004fc11e
stkerchev <stoyan.kerchev@gmail.com>
2023-11-12 01:00:48 +0200

    H2: Connecting to a Slow Payment Processor with Active Job - new System test settings

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,16 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1024, 768] do |options|
+    #options.add_argument('headless')
+    #options.add_argument('--disable-extensions')
+    #options.add_argument('--disable-gpu')
+    #options.add_argument('--disable-dev-shm-usage')
+    #options.add_argument('--no-sandbox')
+    #options.add_argument('--disable-infobars')
+    #options.add_argument('--disable-dev-shm-usage')
+    #options.add_argument('--no-sandbox')
+    options.add_argument('start-maximized')
+    options.add_argument('--remote-debugging-port=9222')
+  end
 end
